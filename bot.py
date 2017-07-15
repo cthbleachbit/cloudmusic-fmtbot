10a835f09bb067cfc45afae9aa2731421ef932a4
James Chai <cth451@gmail.com>
2017-07-15 23:28:22 +0800

    easter egg

--- bot.py
+++ bot.py
@@ -33,6 +33,8 @@ def link_handler_routine(bot, update):
 	except:
 		if chat_id > 0:
 			send_async(bot, chat_id, text = u"这啥？")
+		elif chat_id == -1001140876026:
+			send_async(bot, chat_id, text = u"yoyo 好看")
 		error(bot, update, "Possibly not an valid link:")
 		error(bot, update, update.message.text)
 		return
