9f18ccb9e1193e3b4bf7f60e8e41b39f882e3efd
James Chai <cth451@gmail.com>
2017-07-18 21:21:36 +0800

    even more documentation

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ Netease Cloudmusic Telegram sharing formatting bot
 # What is it?
 Netease Cloudmusic sharing generates __extra amount__ of junk text when sharing songs. This bot generates a photo message with album art and song info.
 
+It is also capable of sharing songs from a group to a channel, see `shared_vars.py` for details.
+
 # To run
 Store your Bot API token in `auth.py`, install the whole bunch of dependencies for python 3 and run `bot.py`
+
+# Commands available
+
+* `help`, `start` triggers a help message
+* `version` sends current git revision
+* `getuid`, `getcid` retrives numerical user id and chat id
 
 # Deps
 * python-telegram-bot
--- shared_vars.py
+++ shared_vars.py
@@ -6,11 +6,16 @@ from auth import TOKEN
 
 updater = Updater(token=TOKEN, workers=16)
 dispatcher = updater.dispatcher
+
+# When a invalid link appears in the special group, the bot will reply with the
+# easter egg message.
+#
+# When a valid link from the special group has been parsed and prepared, the bot
+# sends the share to the special channel in addition to a regular reply to the
+# link.
+#
 # the special group chat id and channel chat id
-# set to 0 to disable
-# the bot will activate special functions for this group:
-#	activates easter egg
-#	repost share from the group to the channel automatically
+# set to 0 to disable these extra functions
 special_group = 0
 special_channel = 0
 easter_egg_msg = u""
