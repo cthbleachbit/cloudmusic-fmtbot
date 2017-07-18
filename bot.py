8bb36e1645c181791e8ed98a0e9cc90b795d5680
James Chai <cth451@gmail.com>
2017-07-18 22:02:08 +0800

    yeah forget 'bout time for I use systemd

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from shared_vars import *
 import info_commands
 
 logging.basicConfig(
-		format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+		format='%(name)s - %(levelname)s - %(message)s',
 		level=logging.INFO)
 logger = logging.getLogger(__name__)
 
